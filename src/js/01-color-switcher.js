082f6640bcc6b9b29fc0727c6c8b395726207215
EGOR-ONIX <127556210+EGOR-ONIX@users.noreply.github.com>
2023-08-07 01:00:42 +0300

    first task + second 50/50

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+let intervalId;
 
+refs.startBtn.addEventListener('click', startGenerationColor);
+refs.stopBtn.addEventListener('click', stopGenerationColor);
+
+function startGenerationColor(event) {
+  event.currentTarget.setAttribute('disabled', true);
+
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+
+  intervalId = setInterval(() => {
+    color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopGenerationColor() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
